03614268b14c9f804cb66401a5894e473f882d54
Phil Kim <philipkim206@gmail.com>
2020-06-15 12:38:28 -0700

    w9-adds adaptive ui for diff states & configs, inc. tablet

--- app/src/main/java/com/philkim/ad340/forecast/WeeklyForecastFragment.kt
+++ app/src/main/java/com/philkim/ad340/forecast/WeeklyForecastFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -29,6 +31,8 @@ class WeeklyForecastFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_weekly_forecast, container, false)
+        val emptyText = view.findViewById<TextView>(R.id.emptyText)
+        val progressBar = view.findViewById<ProgressBar>(R.id.progressBar)
 
         val zipcode = arguments?.getString(KEY_ZIPCODE) ?: ""
 
@@ -43,6 +47,9 @@ class WeeklyForecastFragment : Fragment() {
 
         // Create the observer which updates the UI in response to forecast updates
         val weeklyForecastObserver = Observer<WeeklyForecast> { weeklyForecast ->
+            emptyText.visibility = View.GONE
+            progressBar.visibility = View.GONE
+
             // update our list adapter
             dailyForecastAdapter.submitList(weeklyForecast.daily)
         }
@@ -56,7 +63,10 @@ class WeeklyForecastFragment : Fragment() {
         locationRepository = LocationRepository(requireContext())
         val savedLocationObserver = Observer<Location> { savedLocation ->
             when (savedLocation) {
-                is Location.Zipcode -> forecastRepository.loadWeeklyForecast(savedLocation.zipcode)
+                is Location.Zipcode -> {
+                    progressBar.visibility = View.VISIBLE
+                    forecastRepository.loadWeeklyForecast(savedLocation.zipcode)
+                }
             }
         }
         locationRepository.savedLocation.observe(viewLifecycleOwner, savedLocationObserver)
